06470d224cc360ebf136729a8a64bbedc659e3ce
byeonyong <qusdyd0905@naver.com>
2023-01-03 00:00:12 +0900

    시큐리티 설정

--- src/main/java/com/by/securitystudy/controller/IndexController.java
+++ src/main/java/com/by/securitystudy/controller/IndexController.java
@@ -2,6 +2,7 @@ package com.by.securitystudy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class IndexController {
@@ -9,5 +10,36 @@ public class IndexController {
     @GetMapping({ "", "/" })
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public @ResponseBody String manager(){
+        return "manager";
+    }
+
+//    SecurityConfig 클래스 생성전에는 스프링시큐리티가 낚아챘는데 사용후에는 정상적으로 이 컨트롤러를 사용함 
+    @GetMapping("/login")
+    public @ResponseBody String login(){
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public @ResponseBody String join(){
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody String joinProc(){
+        return "회원가입 완료됨!";
     }
 }
